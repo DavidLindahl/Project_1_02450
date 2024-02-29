cdbe971c183005aef500e37c587711b2c361bb5f
David Lindahl <s234817@dtu.dk>
2024-02-29 14:00:30 +0100

    sidste lort

--- attributes_plot.py
+++ attributes_plot.py
@@ -1,45 +1,12 @@
-# Exercise 4.2.5
-'''This is the m x m plots.
-Explained: This plots all of your attributes against eachother.
-This can be used to get a visualisation of ALL attributes, whereafter
-you can compare the plots and see, which most likely has a chance of 
-succes with linear regression.'''
-# requires data from exercise 4.2.1
+import matplotlib.pyplot as plt
+import seaborn as sns
 from dataloader import *
-from matplotlib.pyplot import (
-    figure,
-    legend,
-    plot,
-    show,
-    subplot,
-    xlabel,
-    xticks,
-    ylabel,
-    yticks,
-    title
-)
 
-figure(figsize=(14, 9))
-for m1 in range(M):
-    for m2 in range(M):
-        subplot(M, M, m1 * M + m2 + 1)
-        for c in range(C):
-            class_mask = y == c
-            plot(np.array(X[class_mask, m2]), np.array(X[class_mask, m1]), ".")
-            if m1 == M - 1:
-                xlabel(attributeNames[m2])
-            else:
-                xticks([])
-            if m2 == 0:
-                ylabel(attributeNames[m1])
-            else:
-                yticks([])
-            # ylim(0,X.max()*1.1)
-            # xlim
-            # (0,X.max()*1.1)
-# legend(classNames)
-title('Correlation of Attributes', y=1.05, size=15)
-
-show()
-
-print("Ran Exercise 4.2.5")
+train_data = data
+plt.figure(figsize=(12,10))
+sns.pairplot(train_data,hue="target")
+plt.title("Looking for Insights in Data")
+plt.legend("target")
+plt.tight_layout()
+plt.plot()
+plt.show()
