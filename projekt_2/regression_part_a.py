27014fe789e2b3d935def6abb9e7726565681b06
mbrochlips <m.brochlips@gmail.com>
2024-04-10 12:45:06 +0200

    fix reg a

--- projekt_2/regression_part_a.py
+++ projekt_2/regression_part_a.py
@@ -4,32 +4,32 @@ import sklearn.linear_model as lm
 from sklearn import model_selection
 from sklearn.metrics import mean_squared_error
 A = 100
-K = len(y)
+K = 10
 error_per_fold = np.zeros(K)
 error_per_aplha = np.zeros(A)
 i = 0
-CV = model_selection.KFold(K, shuffle=True)
+CV = model_selection.KFold(K, shuffle=False)
 for alpha in np.linspace(0, 40, A):
     j = 0
-    model = lm.Ridge(alpha=alpha)
     for train_index, test_index in CV.split(st_X, y):
-        X_train = st_X[train_index]
+        X_train = st_X[train_index, :]
         y_train = y[train_index]
-        X_test = st_X[test_index]
+        X_test = st_X[test_index, :]
         y_test = y[test_index]
+        model = lm.Ridge(alpha=alpha)
         # Fit ordinary least squares regression model
         model.fit(X_train, y_train)
 
         # Predict y
         y_est = model.predict(X_test)
-        error_per_fold[j] = mean_squared_error(y_est, y_test)
+        error_per_fold[j] = mean_squared_error(y_test, y_est)
         j += 1
     error_per_aplha[i] = np.average(error_per_fold)
     i += 1
 
 # Display scatter plot
 plt.figure()
-plt.plot(np.linspace(0, 40, A), error_per_aplha, ".")
+plt.plot(np.linspace(0, 40, A), error_per_aplha, "o-")
 plt.xlabel(f"alpha")
 plt.ylabel(f"mse")
 plt.savefig("plots/error_per_alpha.png")
